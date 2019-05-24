89093305f10ead5d5c9ec642da6fbccfae922ca2
mamluka <david@concom.io>
2019-05-24 19:10:10 +0300

    Beta 1

--- themes/main/src/scripts/index.js
+++ themes/main/src/scripts/index.js
@@ -1,12 +1,13 @@
-
 import '../../../../node_modules/jquery.mmenu/dist/jquery.mmenu.all.css';
-import '../../../../node_modules/jquery-modal/jquery.modal.css';
+// import '../../../../node_modules/jquery-modal/jquery.modal.css';
 
 import '../styles/index.scss';
 
 import $ from '../../../../node_modules/jquery/dist/jquery';
 import '../../../../node_modules/jquery.mmenu/dist/jquery.mmenu.all';
-import '../../../../node_modules/jquery-modal/jquery.modal';
+// import '../../../../node_modules/jquery-modal/jquery.modal';
+import '../../../../node_modules/sticky-sidebar/src/sticky-sidebar';
+
 
 window.jQuery = $;
 
@@ -26,22 +27,41 @@ $(function () {
         }
     });
 
-    var unlock = function () {
-        let items = window.Config.offer_ids;
 
-        let offer = items[Math.floor(Math.random() * items.length)];
-        let loadingDiv = $("<div class='loading'></div>");
+    // var unlock = function () {
+    //     let items = window.Config.offer_ids;
+    //
+    //     let offer = items[Math.floor(Math.random() * items.length)];
+    //     let loadingDiv = $("<div class='loading'></div>");
+    //
+    //     $('body').prepend(loadingDiv);
+    //
+    //     $('#offer').attr('src', `https://govice.online/click?offer_id=${offer[0]}&affiliate_id=512&sub_id1=&link_id=${offer[1]}`);
+    //
+    //     setTimeout(() => {
+    //         loadingDiv.remove()
+    //     }, 5000)
+    // };
+    //
+    // $('#verify-modal').on($.modal.OPEN, function () {
+    //     unlock();
+    // });
 
-        $('body').prepend(loadingDiv);
+    if ($('.sidebar').length) {
+        new StickySidebar('.sidebar', {
+            topSpacing: 20,
+            bottomSpacing: 200,
+            containerSelector: '.main-content',
+            innerWrapperSelector: '.sidebar__inner'
+        });
+    }
 
-        $('#offer').attr('src', `https://govice.online/click?offer_id=${offer[0]}&affiliate_id=512&sub_id1=&link_id=${offer[1]}`);
-
-        setTimeout(() => {
-            loadingDiv.remove()
-        }, 5000)
-    };
-
-    $('#verify-modal').on($.modal.OPEN, function () {
-        unlock();
+    $('.search-icon').click(function () {
+        $('#search-box-modal').toggleClass('is-active');
     });
+
+    $('#search-box-close').click(function() {
+        $('#search-box-modal').toggleClass('is-active');
+    });
+
 });
